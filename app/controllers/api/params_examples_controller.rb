1595ee3ffcef5565147b83d91b14e0d6f955a42e
sbatsch <sbatsch2@gmail.com>
2020-01-16 12:23:43 -0600

    revised

--- app/controllers/api/params_examples_controller.rb
+++ app/controllers/api/params_examples_controller.rb
@@ -4,4 +4,29 @@ class Api::ParamsExamplesController < ApplicationController
     @other = params["other"]
     render 'query_params_view.json.jb'
   end
+
+  def segment_params_action
+    @message = params[:wildcard]
+    render 'segment_params_view.json.jb'
+  end 
+
+  
+  def guess_query
+    @guess = params[:wildcard].to_i
+    hidden_number = 42
+
+    if @guess > hidden_number
+       @message = "Woah, too high, Go lower!"
+    elsif @guess < hidden_number
+      @message = "Come up a bit, guess higher next time."
+    else
+      @message = "and that number was just right."
+    end 
+      render 'guess_query.json.jb'
+  end 
+
+  def form_params_action
+    @message = params[:message]
+    render 'form_params_view.json.jb'
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
  namespace :api do
    get 'query_params_url' => 'params_examples#query_params_action'
+   get '/segment_params_url' => 'params_examples#segment_params_action' 
+   get '/guess_query/:wildcard' => 'params_examples#guess_query'
+   post '/body_params_url' => 'params_examples#form_params_action'
   end
 end 
